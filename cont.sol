1d8e5371d6cd8601f91efcab1a93143e2a144963
werytens <maxim.lipatov06@gmail.com>
2023-09-26 20:47:31 +0300

    transactions render add 2

--- cont.sol
+++ cont.sol
@@ -29,31 +29,36 @@ contract Transfer {
 
     constructor() {
         // Admins
-
-        user_mapping[0xdD870fA1b7C4700F2BD7f44238821C26f7392148] = (User("Maxim", 1));
-        users.push(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
-        user_mapping[0x583031D1113aD414F02576BD6afaBfb302140225] = (User("Dima", 1));
-        users.push(0x583031D1113aD414F02576BD6afaBfb302140225);
+        user_mapping[msg.sender] = (User("Maxim", 1));
+        users.push(msg.sender);
+        // user_mapping[0xdD870fA1b7C4700F2BD7f44238821C26f7392148] = (User("Maxim", 1));
+        // users.push(0xdD870fA1b7C4700F2BD7f44238821C26f7392148);
+        // user_mapping[0x583031D1113aD414F02576BD6afaBfb302140225] = (User("Dima", 1));
+        // users.push(0x583031D1113aD414F02576BD6afaBfb302140225);
 
         // Users
-        user_mapping[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = (User("Alex", 0));
-        users.push(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
-        user_mapping[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = (User("Daniel", 0));
-        users.push(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
-        user_mapping[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = (User("Ivan", 0));
-        users.push(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
-        user_mapping[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB] = (User("Anna", 0));
-        users.push(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
+        // user_mapping[0x5B38Da6a701c568545dCfcB03FcB875f56beddC4] = (User("Alex", 0));
+        // users.push(0x5B38Da6a701c568545dCfcB03FcB875f56beddC4);
+        // user_mapping[0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2] = (User("Daniel", 0));
+        // users.push(0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2);
+        // user_mapping[0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db] = (User("Ivan", 0));
+        // users.push(0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db);
+        // user_mapping[0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB] = (User("Anna", 0));
+        // users.push(0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB);
     }
 
     function registration(string memory name, address user_address) public {
-        // require(user_mapping[msg.sender].status == 1, "u not admin");
+        require(user_mapping[msg.sender].status == 1, "u not admin");
         user_mapping[user_address] = (User(name, 0));
         users.push(user_address);
     }
 
     function getAllUsers() public returns (address[] memory) {
         return users;
+    }
+
+    function getAllTransfers() public returns (Transfer[] memory) {
+        return transfers;
     }
 
     function add_transfer(uint value, address target, string memory code) public payable {
